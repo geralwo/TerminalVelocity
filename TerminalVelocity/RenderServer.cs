4723f11c275ec51b2312666327e17667fddb887e
geralwo <geralwiewienciers@gmail.com>
2024-04-09 19:37:43 +0200

    error checking fixed

--- TerminalVelocity/RenderServer.cs
+++ TerminalVelocity/RenderServer.cs
@@ -12,6 +12,8 @@ public class RenderServer
 	private static RenderServer singleton = new RenderServer();
 	private Dictionary<Vec2i, SceneObject> screen_buffer = new Dictionary<Vec2i, SceneObject>();
     private List<SceneObject> registered_buffer = new List<SceneObject>();
+
+    private SortedSet<int> Z_INDICES = new SortedSet<int>();
 	public static RenderServer Instance { 
 		get {
             if (singleton == null)
@@ -31,6 +33,7 @@ public class RenderServer
         {
             return false;
         }
+        Z_INDICES.Add(obj.ZIndex);
         registered_buffer.Add(obj);
         return true;
 	}
@@ -53,10 +56,10 @@ public class RenderServer
 
     public void DrawBuffer()
     {
-        
-
         foreach(Vec2i idx in screen_buffer.Keys)
         {   
+            if(idx.x < 0 || idx.y < 0 || idx.x > Console.WindowWidth || idx.y > Console.WindowHeight)
+                continue;
             Console.SetCursorPosition(idx.x, idx.y);
             Console.ForegroundColor = screen_buffer[idx].ForegroundColor;
             Console.BackgroundColor = screen_buffer[idx].BackgroundColor;
@@ -65,11 +68,15 @@ public class RenderServer
             Console.ResetColor();
         }
         render();
-        }
+    }
         
     public void render()
     {
         var screen_buffer_copy = new Dictionary<Vec2i, SceneObject>();
+        foreach(int i in Z_INDICES)
+        {
+            
+        }
         foreach(SceneObject pixel in registered_buffer)
         {
             if(!screen_buffer_copy.ContainsKey(pixel.Position))
@@ -77,18 +84,28 @@ public class RenderServer
                 screen_buffer_copy.Add(pixel.Position, pixel);
                 continue;
             }
-            if(pixel.ZIndex > screen_buffer_copy[pixel.Position].ZIndex)
-            {
-                screen_buffer_copy[pixel.Position] = pixel;
-            }
+            screen_buffer_copy[pixel.Position] = pixel;
         }
+                
+
         foreach(Vec2i pos in screen_buffer.Keys)
         {
+            if(pos.x < 0 || pos.y < 0 || pos.x > Console.WindowWidth || pos.y > Console.WindowHeight)
+                continue;
             if(!screen_buffer_copy.ContainsKey(pos))
             {
                 Console.SetCursorPosition(pos.x,pos.y);
                 //Console.BackgroundColor = ConsoleColor.Red;
                 Console.Write(" ");
+            }
+            else
+            {
+                if(screen_buffer_copy[pos].ZIndex > screen_buffer[pos].ZIndex)
+                {
+                    Console.SetCursorPosition(pos.x,pos.y);
+                    //Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Write(" ");
+                }
             }
         }
         screen_buffer = screen_buffer_copy;
