5081b506442d21fbb2f0e5fc13fc06e4eaa6a54d
geralwo <geralwiewienciers@gmail.com>
2024-04-09 22:15:15 +0200

    update max fps

--- TerminalVelocity/DefaultSettings.cs
+++ TerminalVelocity/DefaultSettings.cs
@@ -5,7 +5,7 @@ public class DefaultSettings : GameSettings
     public string player_char = "@";
     public Vec2i lvl_size = new Vec2i(16,16);
 
-    public int MaxFps = 60;
+    public int MaxFps = 120;
 
     public bool AudioEnabled = false;
     public DefaultSettings()
