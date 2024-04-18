c8ef7e4ffd173e08170542230ed2dd896e1ca8ea
geralwo <geralwiewienciers@gmail.com>
2024-04-18 16:49:18 +0200

    added comments

--- TerminalVelocity/RenderServer.cs
+++ TerminalVelocity/RenderServer.cs
@@ -10,40 +10,43 @@ namespace TerminalVelocity;
 
 public class RenderServer
 {
-	private static RenderServer singleton = new RenderServer();
-	private Dictionary<Vec2i, SceneObject> screen_buffer = new Dictionary<Vec2i, SceneObject>();
+    private static RenderServer singleton = new RenderServer();
+    private Dictionary<Vec2i, SceneObject> screen_buffer = new Dictionary<Vec2i, SceneObject>();
     private List<SceneObject> registered_buffer = new List<SceneObject>();
-	public static RenderServer Instance { 
-		get {
+    public static RenderServer Instance
+    {
+        get
+        {
             if (singleton == null)
             {
                 singleton = new RenderServer();
             }
             return singleton;
         }
-	}
-	private RenderServer(){ 
-	}
+    }
+    private RenderServer()
+    {
+    }
 
-	public bool AddItem(SceneObject obj)
-	{
+    public bool AddItem(SceneObject obj)
+    {
         if (registered_buffer.Contains(obj))
         {
             return false;
         }
         registered_buffer.Add(obj);
         return true;
-	}
+    }
 
-	public bool RemoveItem(SceneObject obj)
-	{
-        if(registered_buffer.Contains(obj))
+    public bool RemoveItem(SceneObject obj)
+    {
+        if (registered_buffer.Contains(obj))
         {
             registered_buffer.Remove(obj);
             return true;
         }
         return false;
-	}
+    }
 
     public int Count()
     {
@@ -58,21 +61,65 @@ public class RenderServer
             // Console.ForegroundColor = screen_buffer[idx].ForegroundColor;
             // Console.BackgroundColor = screen_buffer[idx].BackgroundColor;
 
-            screen_buffer.TryGetValue(idx, out SceneObject v);
+            screen_buffer.TryGetValue(idx, out SceneObject v); // rider said this is better
             if (v != null)
             {
                 Console.ForegroundColor = v.ForegroundColor;
                 Console.BackgroundColor = v.BackgroundColor;
+                Console.Write(v.Display);
             }
 
-            Console.Write(screen_buffer[idx].Display);
+            // Console.Write(screen_buffer[idx].Display);
             // Console.Write(screen_buffer[idx].name);
             // Console.Write(screen_buffer[idx].GetType());
             // Console.Write(screen_buffer[idx].ZIndex);
             Console.ResetColor();
         }
-
         render();
+    }
+
+    public void render()
+    {
+        var new_screen_buffer = new Dictionary<Vec2i, SceneObject>();
+
+        foreach (var obj in registered_buffer.Where(obj => obj.Visible))
+        {
+            new_screen_buffer.TryAdd(obj.Position, obj);
+            {
+                // if the current obj.ZIndex is higher than the pixel in the buffer we overwrite it
+                if (obj.ZIndex > new_screen_buffer[obj.Position].ZIndex)
+                {
+                    new_screen_buffer[obj.Position] = obj;
+                }
+            }
+            // if the old screen buffer has a key for this position 
+            // and the object in the new buffer is the same, 
+            // we dont need to render it again because its already there
+            if (screen_buffer.ContainsKey(obj.Position) && obj.id == new_screen_buffer[obj.Position].id)
+            {
+                // the obj did not move, so we remove it from the new screen buffer
+                screen_buffer.Remove(obj.Position);
+            }
+        }
+
+
+        // clean up screen by removing pixels left in the old screen buffer
+        foreach (var pos in screen_buffer.Keys)
+        {
+            Console.SetCursorPosition(pos.x, pos.y);
+            for (int i = 0; i < screen_buffer[pos].Display.Length; i++) // bug: this is a refernce to an object. the display length could have changed
+            {
+                Console.Write(" ");
+            }
+            if(pos.x > Console.WindowWidth || pos.y > Console.WindowHeight)
+            {
+                Console.SetCursorPosition(pos.x,pos.y);
+                Console.Write(" ");
+            }
+        }
+        // now we just add all pixels to the buffer to render
+        // todo: we dont need to add all pixels, only the pixels that have changed
+        screen_buffer = new_screen_buffer;
     }
 
     internal void clear_scene()
@@ -80,34 +127,5 @@ public class RenderServer
         Console.Clear();
         screen_buffer = new Dictionary<Vec2i, SceneObject>();
         registered_buffer = new List<SceneObject>();
-    }
-        
-    public void render()
-    {
-        var new_screen_buffer = new Dictionary<Vec2i, SceneObject>();
-        foreach (var pixel in registered_buffer.Where(pixel => pixel.Visible))
-        {
-            if(screen_buffer.ContainsKey(pixel.Position) && pixel.id == screen_buffer[pixel.Position].id) // obj did not move, so we remove it from the old screen to reduce cleanup loops
-            {
-                screen_buffer.Remove(pixel.Position);
-            }
-
-            new_screen_buffer.TryAdd(pixel.Position, pixel);
-
-            if(pixel.ZIndex > new_screen_buffer[pixel.Position].ZIndex)
-            {
-                new_screen_buffer[pixel.Position] = pixel;
-            }
-        }
-        foreach (var pos in screen_buffer.Keys)
-        {
-            Console.SetCursorPosition(pos.x,pos.y);
-            for(int i = 0; i < screen_buffer[pos].Display.Length; i++)
-            {
-                Console.Write(" ");
-            }
-        }
-
-        screen_buffer = new_screen_buffer;
     }
 }
