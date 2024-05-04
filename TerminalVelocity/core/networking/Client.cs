a02de28b0c48cc89c72551b7db7ba50bc20264cd
geralwo <geralwiewienciers@gmail.com>
2024-05-05 01:35:50 +0200

    initial

---
+++ TerminalVelocity/core/networking/Client.cs
@@ -0,0 +1,6 @@
+namespace TerminalVelocity.Networking;
+
+public class Client
+{
+    public Client() { throw new NotImplementedException("Client not implemented"); }
+}
---
+++ TerminalVelocity/core/networking/Server.cs
@@ -0,0 +1,7 @@
+namespace TerminalVelocity.Networking;
+
+public class Server
+{
+    public Server()
+    { throw new NotImplementedException("Server is not implemented"); }
+}
