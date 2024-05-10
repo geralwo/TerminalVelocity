e4d9c8ecf18c188617cfcb321c0e3083617dab88
geralwo <geralwiewienciers@gmail.com>
2024-05-10 03:31:37 +0200

    fixed RenderTests

--- RenderTests/scenes/RenderTestMenu.cs
+++ RenderTests/scenes/RenderTestMenu.cs
@@ -4,8 +4,14 @@ public class RenderTestMenu : StartScene
 {
     public RenderTestMenu(string _name = "RenderTestMenu") : base(_name)
     {
-        SceneObject Start = new SceneObject("Start Tests");
-        Start.ProcessAction = () => Game.CurrentScene = new RenderTest();
-        MenuList.AddChild(Start, 0);
+        // SceneObject Start = new SceneObject("Start Tests");
+        // Start.ProcessAction = () =>Game.CurrentScene = new RenderTest();
+        // MenuList.AddChild(Start, 0);
     }
+    public override void OnStart()
+    {
+        base.OnStart();
+        Game.CurrentScene = new RenderTest();
+    }
+
 }
